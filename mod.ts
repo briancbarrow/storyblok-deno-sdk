5b7728e68d4068f12b73964ebd68e3b33b2f8dd9
Brian Barrow <briancbarrow@gmail.com>
2022-08-21 13:42:58 -0600

    small update

--- mod.ts
+++ mod.ts
@@ -1,7 +1,9 @@
 import { Storyblok } from "./storyblok.ts";
 export default function storyblok(token: string): Storyblok {
   try {
-    return new Storyblok(token);
+    const storyblok = new Storyblok(token);
+    console.log("Storyblok SDK initialized");
+    return storyblok;
   } catch (err) {
     throw new Error("Storyblok connection failed: " + err);
   }
